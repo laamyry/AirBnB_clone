df97f3cdb3d06906c573c40183fbb0d5a3c48284
laamyry <slamiri10@gmail.com>
2023-11-23 23:04:30 +0100

    add

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 '''Module for the BaseModel class.'''
 import uuid
 from datetime import datetime as date
-
+from models import storage
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
@@ -10,14 +10,14 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
-                        setattr(self, key,
-                                date.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                        setattr(self, key, date.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
                     else:
                         setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = date.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__,
@@ -25,6 +25,7 @@ class BaseModel:
 
     def save(self):
         self.updated_at = date.now()
+        storage.save()
 
     def to_dict(self):
         model_dict = self.__dict__.copy()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,3 +1,34 @@
 #!/usr/bin/python3
-'''Be pycodestyle compliant!'''
-print("Be pycodestyle compliant!")
+'''Module for the FileStorage class.'''
+import json
+from os.path import exists
+from models.base_model import BaseModel as base
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        objects_dict = {}
+        for key, obj in self.__objects.items():
+            objects_dict[key] = obj.to_dict()
+        with open(self.__file_path, 'w', encoding="utf-8") as file:
+            json.dump(objects_dict, file)
+
+    def reload(self):
+        if exists(self.__file_path):
+            with open(self.__file_path, 'r', encoding="utf-8") as file:
+                objects_dict = json.load(file)
+                for key, obj_dict in objects_dict.items():
+                    class_name, obj_id = key.split('.')
+                    obj_dict['__class__'] = class_name
+                    obj = eval(class_name)(**obj_dict)
+                    self.__objects[key] = obj
